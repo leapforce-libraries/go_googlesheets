859daf3d5d25414ea997edc7f9822c8236a21908
caselongo <caselongo@gmail.com>
2021-07-22 11:44:23 +0200

    x

--- Service.go
+++ Service.go
@@ -9,35 +9,70 @@ import (
 )
 
 const (
-	APIName string = "GoogleSheets"
-	APIURL  string = "https://sheets.googleapis.com/v4"
+	apiName string = "GoogleSheets"
+	apiURL  string = "https://sheets.googleapis.com/v4"
 )
 
-// Service stores Service configuration
-//
 type Service struct {
+	clientID      string
 	googleService *google.Service
 }
 
-// methods
-//
-func NewService(clientID string, clientSecret string, scope string, bigQueryService *bigquery.Service) *Service {
-	config := google.ServiceConfig{
-		APIName:      APIName,
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		Scope:        scope,
+type ServiceConfig struct {
+	ClientID     string
+	ClientSecret string
+}
+
+func NewService(serviceConfig *ServiceConfig, bigQueryService *bigquery.Service) (*Service, *errortools.Error) {
+	if serviceConfig == nil {
+		return nil, errortools.ErrorMessage("ServiceConfig must not be a nil pointer")
 	}
 
-	googleService := google.NewService(config, bigQueryService)
+	if serviceConfig.ClientID == "" {
+		return nil, errortools.ErrorMessage("ClientID not provided")
+	}
 
-	return &Service{googleService}
+	if serviceConfig.ClientSecret == "" {
+		return nil, errortools.ErrorMessage("ClientSecret not provided")
+	}
+
+	googleServiceConfig := google.ServiceConfig{
+		APIName:      apiName,
+		ClientID:     serviceConfig.ClientID,
+		ClientSecret: serviceConfig.ClientSecret,
+	}
+
+	googleService, e := google.NewService(&googleServiceConfig, bigQueryService)
+	if e != nil {
+		return nil, e
+	}
+
+	return &Service{
+		clientID:      serviceConfig.ClientID,
+		googleService: googleService,
+	}, nil
 }
 
 func (service *Service) url(path string) string {
-	return fmt.Sprintf("%s/%s", APIURL, path)
+	return fmt.Sprintf("%s/%s", apiURL, path)
 }
 
-func (service *Service) InitToken() *errortools.Error {
-	return service.googleService.InitToken()
+func (service *Service) InitToken(scope string, accessType *string, prompt *string, state *string) *errortools.Error {
+	return service.googleService.InitToken(scope, accessType, prompt, state)
+}
+
+func (service *Service) APIName() string {
+	return apiName
+}
+
+func (service *Service) APIKey() string {
+	return service.clientID
+}
+
+func (service *Service) APICallCount() int64 {
+	return service.googleService.APICallCount()
+}
+
+func (service *Service) APIReset() {
+	service.googleService.APIReset()
 }
